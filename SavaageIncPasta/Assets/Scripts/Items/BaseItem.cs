7af4fe246567892188f276ee57abeaf622083ab0
t7042361 <t7042361@TU46964>
2019-02-05 15:46:08 +0000

    Added Base Item

---
+++ SavaageIncPasta/Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    eCONSUMABLE,
+    eARMOUR,
+    eWEAPON
+}
+
+public abstract class BaseItem : ScriptableObject {
+    public string Item;
+    public string Description;
+
+    protected ItemType ItemType; //Type of base item
+}
