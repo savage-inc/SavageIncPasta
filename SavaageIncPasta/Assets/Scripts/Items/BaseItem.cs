e94cd7863ed9cac2eea4ba755f598c3076f44b3c
t7042361 <t7042361@TU46964>
2019-02-05 16:55:00 +0000

    Added Weapon Base Class Added Armour Base Class

--- SavaageIncPasta/Assets/Scripts/Items/BaseItem.cs
+++ SavaageIncPasta/Assets/Scripts/Items/BaseItem.cs
@@ -9,9 +9,69 @@ public enum ItemType
     eWEAPON
 }
 
-public abstract class BaseItem : ScriptableObject {
+public enum ItemRarity
+{
+    eCOMMON,
+    eUNCOMMON,
+    eRARE,
+    eEPIC,
+    eLEGENDARY
+}
+
+public abstract class BaseItem : ScriptableObject
+{
     public string Item;
     public string Description;
+    public Sprite PreviewSprite;
+    public ItemRarity Rarity;
 
-    protected ItemType ItemType; //Type of base item
+    protected ItemType ItemType;
+}
+
+[CreateAssetMenu(fileName = "Data", menuName = "Items/ConsumableItem", order = 1)]
+public class ConsumableItem : BaseItem
+{
+    public enum Type
+    {
+        eHEALTH,
+        eSTRENGTH,
+        eCONSITUTION,
+        eDEXTERITY
+    }
+
+    public ConsumableItem()
+    {
+        ItemType = ItemType.eCONSUMABLE;
+    }
+
+    public Type ConsumableType;
+    public float EffectAmount;
+}
+
+[CreateAssetMenu(fileName = "Data", menuName = "Items/ArmourItem", order = 1)]
+public class ArmourItem : BaseItem
+{
+    public enum Type
+    {
+        eHEAD,
+        eCHEST,
+        eLEGS,
+    }
+
+    public Type ArmourType;
+    public float Modifiier;
+}
+
+[CreateAssetMenu(fileName = "Data", menuName = "Items/WeaponItem", order = 1)]
+public class WeaponItem : BaseItem
+{
+    public enum Type
+    {
+        eSWORD,
+        eBOW,
+        eSTAFF
+    }
+
+    public Type WeaponType;
+    public float Damage;
 }
