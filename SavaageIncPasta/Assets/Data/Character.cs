bf4e650c7508e550e1da93cd33c3e74f7ce05f80
GRIERSON <T7094911@live.tees.ac.uk>
2019-02-12 16:35:37 +0000

    joe's a pain

---
+++ SavaageIncPasta/Assets/Data/Character.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Data", menuName = "Class")]
+
+public class Character : ScriptableObject
+{
+    public string ClassName;
+    public int Health;
+    public int Mana;
+    public int Strength;
+    public int Constitution;
+    public int Dexterity;
+    public int Intelligence;
+    public int Charisma;
+}
