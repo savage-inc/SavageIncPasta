a565a6b490131f89c69cc8b3af59d49977b8f1fd
James Hadley <james.hadley1@live.co.uk>
2019-02-19 16:45:33 +0000

    Movement complete

---
+++ SavaageIncPasta/Assets/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D _rb;
+    private Vector2 _moveVelocity;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _moveVelocity = moveInput.normalized * Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _moveVelocity * Time.fixedDeltaTime);
+    }
+}
---
+++ SavaageIncPasta/Assets/Scripts/World/WorldInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void TalkToNPC()
+    {
+        // Add code to allow for interaction with NPCs
+    }
+
+    private void PickUpItems()
+    {
+        // Add code to allow for items to be picked up
+    }
+
+}
