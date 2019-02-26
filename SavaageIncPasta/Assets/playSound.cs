c36969a2dafeadd468522c63b88db388b2db0c58
COOPER <P4122433@live.tees.ac.uk>
2019-02-26 16:08:42 +0000

    Sound system sprint 1

---
+++ SavaageIncPasta/Assets/playSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSound : MonoBehaviour {
+	public SoundManager Sound;
+	public int clip;
+	// Use this for initialization
+	void Start () {
+		print("dog barking");
+		Sound.PlaySFX(Sound.SfxClips[clip], GetComponent<AudioSource>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
