471fb1a36d98b51a21a957b055daa13f45d9576e
HADLEY <T7124647@live.tees.ac.uk>
2019-03-12 17:09:58 +0000

    Party UI

---
+++ SavaageIncPasta/Assets/Scripts/Comfort.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Comfort : MonoBehaviour {
+
+    // Variables
+    int ComfortLvl = 100;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // If ComfortLvl is < 100 then return to tarven
+       // ComfortLvl is decreased by value constitution * 3 after battles
+       // If ComfortLvl is <= 1 then regain if at tarven
+       // If all party ComfortLvl is 0 game over
+       // If potion used then ComfortLvl is increased
+    }
+    
+}
---
+++ SavaageIncPasta/Assets/Scripts/UI/PartyMechanics.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PartyManager : MonoBehaviour {
+    public Comfort comfortScript; // Access Comfort class
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		// If character choosen
+        // Then they are used for battle
+        // If not choosen then they are left at the tarven to regain comfort
+	}
+}
