4146e45efb17d54f6d53842273d3e38dd0a090b4
James <james.hadley1@live.co.uk>
2019-03-29 14:42:17 +0000

    Swap update

---
+++ SavaageIncPasta/Assets/Scripts/UI/CharacterButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CharacterButton : MonoBehaviour
+{
+    public Button SwapButton, CompareButton, CloseButton;
+    public CharacterComparison CharacterCompare;
+    public CharacterComparison CharacterSwap;
+    public Character Character;
+    public int CharacterIndex = 0;
+
+
+    private void Awake()
+    {
+        Character = FindObjectOfType<PlayerManager>().Characters[CharacterIndex];
+    }
+
+    private void Update()
+    {
+        Character = FindObjectOfType<PlayerManager>().Characters[CharacterIndex];
+    }
+
+    public void ShowButtons()
+    {
+        if (!SwapButton.gameObject.activeInHierarchy)
+        {
+            SwapButton.gameObject.SetActive(true);
+            CompareButton.gameObject.SetActive(true);
+            CloseButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            SwapButton.gameObject.SetActive(false);
+            CompareButton.gameObject.SetActive(false);
+            CloseButton.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetToCompareMode()
+    {
+        // Compares party stats
+
+        CharacterCompare.character = Character;    
+
+    }
+
+    public void SetToSwapMode()
+    {
+        // Switch party character
+        CharacterSwap.character = Character;
+    }
+
+}
