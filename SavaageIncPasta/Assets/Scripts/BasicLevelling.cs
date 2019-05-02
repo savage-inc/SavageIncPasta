040ea19daff4acda8b4143800d92432cbdf2e251
HAQUE <S6061420@live.tees.ac.uk>
2019-05-02 10:47:16 +0100

    Balanced the amount of hp, mana and comfort the character gains when levelling up

--- SavaageIncPasta/Assets/Scripts/BasicLevelling.cs
+++ SavaageIncPasta/Assets/Scripts/BasicLevelling.cs
@@ -9,6 +9,7 @@ public class BasicLevelling : MonoBehaviour
     private List<Character> _levelledUpCharacters;
     private PlayerManager _playerManager;
 
+    //Checks if any character has levelled up when the scene changes
     private void Awake()
     {
         _levelledUpCharacters = new List<Character>();
@@ -27,20 +28,24 @@ public class BasicLevelling : MonoBehaviour
         return _levelledUpCharacters.Count;
     }
 
+    //Checks the amount of xp the character has and how much they need to level up
     public void CheckLevels()
     {
         foreach (Character character in _characters)
         {
             int xpNeededForNextLevel = 11 * (character.Level * character.Level) + 30;
-            //character.Experience = 250;
+            character.Experience = 250;
             if (character.Experience >= xpNeededForNextLevel)
             {
-                character.MaxHealth = 11 * (character.Level * character.Level) + 30;
+                character.MaxComfort += character.Constitution * 5 + character.Level * 2;
+                character.Comfort = character.MaxComfort;
+
+                character.MaxHealth += 2 * character.Constitution + 3 * character.Level;
                 character.CurrentHealth = character.MaxHealth;
 
                 if (character.Class == ClassType.eSHAMAN || character.Class == ClassType.eWIZARD)
                 {
-                    character.MaxMana = 11 * (character.Level * character.Level) + 30;
+                    character.MaxMana += character.Level + character.Intelligence - 3;
                     character.CurrentMana = character.MaxMana;
                 }
 
